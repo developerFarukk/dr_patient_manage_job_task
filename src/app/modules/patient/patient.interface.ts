00b28cc12f7c78047a72187a1995b63e924ff403
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-28 01:13:17 +0600

    feat: create user interface

---
+++ src/app/modules/patient/patient.interface.ts
@@ -0,0 +1,25 @@
+import { Model } from "mongoose";
+
+export interface TUser {
+  _id?: string;
+  name: string,
+  email: string;
+  phone: string,
+  password: string;
+  role: 'admin' | 'patient' | 'doctor' ;
+  status: 'in-progress' | 'blocked';
+};
+
+
+export interface UserModel extends Model<TUser> {
+
+  //instance methods for checking if the user exist
+  isUserExistsByCustomId(_id: string): Promise<TUser>;
+
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+
+}
