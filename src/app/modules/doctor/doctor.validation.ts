2ab661638ef51776ac443c61e25b619085617aa3
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-28 06:33:21 +0600

    feat: doctor validation create

---
+++ src/app/modules/doctor/doctor.validation.ts
@@ -0,0 +1,44 @@
+
+
+import { z } from 'zod'
+
+// Patient validation schema
+export const createDoctorValidationSchema = z.object({
+  body: z.object({
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters' })
+      .max(20, { message: 'Password must be less than 20 characters' }),
+    patient: z.object({
+      name: z
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters' })
+        .max(40, { message: 'Name must be less than 40 characters' }),
+      email: z
+        .string()
+        .min(1, { message: 'Email is required.' })
+        .email({ message: 'Please provide a valid email address.' }),
+      phone: z
+        .string()
+        .min(11, { message: 'Number must be at least 11 characters long.' })
+        .max(15, { message: 'Number must be at least 15 characters long.' }),
+      gender: z.enum(['male', 'female', 'other']).optional(),
+      specialization: z
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters' }),
+      
+      hospitalName: z
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters' }),
+      
+      hospitalFloor: z
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters' })
+      
+    }),
+  }),
+})
+
+export const doctorValidation = {
+  createDoctorValidationSchema,
+}
