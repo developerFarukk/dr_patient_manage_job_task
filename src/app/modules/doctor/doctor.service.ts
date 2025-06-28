add4858c91cc4cc2a791d7ed75bf041dc89a5565
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-28 18:09:35 +0600

    feat: add query builder

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -1,17 +1,16 @@
-import AppError from "../../errors/AppError"
-import { TService } from "../doctor-service/doctor-service.interface"
+import AppError from '../../errors/AppError'
+import { TService } from '../doctor-service/doctor-service.interface'
 // import { Service } from "../doctor-service/doctor-service.model"
 import httpStatus from 'http-status'
-import { User } from "../user/user.model"
-import { Service } from "../doctor-service/doctor-service.model"
-
+import { User } from '../user/user.model'
+import { Service } from '../doctor-service/doctor-service.model'
+import QueryBuilder from '../../builder/QueryBuilder'
 
 // CREATE DOCTOR SERVICE
 const createDoctorServiceIntoDB = async (
   userEmail: string,
   payload: Omit<TService, 'doctor'>
 ) => {
-
   // Check User exixtse
   const user = await User.isUserExistsById(userEmail)
   const userId = user?._id
@@ -19,13 +18,12 @@ const createDoctorServiceIntoDB = async (
   if (!userId) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user id is not found !!!')
   }
-  
+
   const serviceData = {
     ...payload,
-    doctor: userId ,
+    doctor: userId,
   }
 
-  
   const service = await Service.create(serviceData)
 
   if (!service) {
@@ -35,6 +33,26 @@ const createDoctorServiceIntoDB = async (
   return service
 }
 
+// get all doctor service
+const getAllDoctorServiceFromDB = async (query: Record<string, unknown>) => {
+
+  const doctorServiceQuery = new QueryBuilder(Bicycle.find(), query)
+    .search(BicycleSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields()
+
+  const meta = await doctorServiceQuery.countTotal()
+  const result = await doctorServiceQuery.modelQuery
+
+  return {
+    meta,
+    result,
+  }
+}
+
 export const DoctorServices = {
   createDoctorServiceIntoDB,
+  getAllDoctorServiceFromDB
 }
