446df1058df6872af531d9b01ecf919facf47f2d
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-29 16:14:36 +0600

    fix: re update readmi md file

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app: Application = express();
 
 // middleware
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://dr-patient-management.vercel.app'], credentials: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
